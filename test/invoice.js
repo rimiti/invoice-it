b35be2247354dad632da437df661fca642c4f76f
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-05-04 16:59:27 +0200

    feat(test) prepare tdd methods

---
+++ test/invoice.js
@@ -0,0 +1,18 @@
+import chai from 'chai'
+chai.should()
+
+describe('Invoice', () => {
+
+  it(`Object attributes`, (done) => {
+    done()
+  })
+
+  it(`Export to HTML`, (done) => {
+    done()
+  })
+
+  it(`Export to PDF`, (done) => {
+    done()
+  })
+
+})
---
+++ test/order.js
@@ -0,0 +1,18 @@
+import chai from 'chai'
+chai.should()
+
+describe('Order', () => {
+
+  it(`Object attributes`, (done) => {
+    done()
+  })
+
+  it(`Export to HTML`, (done) => {
+    done()
+  })
+
+  it(`Export to PDF`, (done) => {
+    done()
+  })
+
+})
