c1121cd4313cb4341ec41f73d58a1e85c7841c69
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-05-04 17:36:56 +0200

    feat(class/base.js) hydrate method created + code indented

--- src/class/base.js
+++ src/class/base.js
@@ -3,19 +3,30 @@ import path from 'path'
 
 export default class Base {
 
-    constructor() {
-        this._template = null
-    }
+  constructor() {
+    this._template = null
+  }
 
-    _compile() {
-        let emailFn = jade.compileFile(path.resolve(this.template))
-    }
+  get template() {
+    return this._template
+  }
 
-    get template() {
-        return this._template;
-    }
+  set template(value) {
+    this._template = value
+  }
 
-    set template(value) {
-        this._template = value;
+  _compile() {
+    let emailFn = jade.compileFile(path.resolve(this.template))
+  }
+
+  /**
+   * @description Hydrate current instance with obj attributes
+   * @param obj
+   * @return {Fiche}
+   */
+  hydrate(obj, attributes) {
+    for (let item of attributes) {
+      this[item] = obj[item]
     }
+  }
 }
