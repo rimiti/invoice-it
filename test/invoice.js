c3cfb55484c6401dbec020ba7f6c66b6b6a7823a
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-05-06 17:30:40 +0200

    chore(test/invoice.js) tests objects attributes

--- test/invoice.js
+++ test/invoice.js
@@ -1,13 +1,100 @@
 import chai from 'chai'
-import Document from '../class'
+import generator from '../lib/generator'
 chai.should()
 
 describe('Invoice', () => {
 
-  it(`Object attributes`, (done) => {
+  it(`Object auto-filled`, (done) => {
+    let invoice = generator.create({
+      company_name: 'Receiver company',
+      first_name: 'Will',
+      last_name: 'Jameson',
+      street_number: '20',
+      street_name: 'Rue Victor Hugo',
+      zip_code: '77340',
+      city: 'Pontault-Combault',
+      country: 'France',
+      phone: '06 00 00 00 00',
+      mail: 'will.jameson@test.com'
+    }, {
+      name: 'Dim Solution',
+      street_number: '15',
+      street_name: 'Rue Jean Jaures',
+      zip_code: '75012',
+      city: 'Paris',
+      country: 'France',
+      phone: '01 00 00 00 00',
+      mail: 'contact@dimsolution.com',
+      website: 'www.dimsolution.com'
+    })
 
-    let document = new Document()
+    invoice.recipient().company_name.should.be.equal('Receiver company')
+    invoice.recipient().first_name.should.be.equal('Will')
+    invoice.recipient().last_name.should.be.equal('Jameson')
+    invoice.recipient().street_number.should.be.equal('20')
+    invoice.recipient().street_name.should.be.equal('Rue Victor Hugo')
+    invoice.recipient().zip_code.should.be.equal('77340')
+    invoice.recipient().city.should.be.equal('Pontault-Combault')
+    invoice.recipient().country.should.be.equal('France')
+    invoice.recipient().phone.should.be.equal('06 00 00 00 00')
+    invoice.recipient().mail.should.be.equal('will.jameson@test.com')
+    invoice.emitter().name.should.be.equal(`Dim Solution`)
+    invoice.emitter().street_number.should.be.equal('15')
+    invoice.emitter().street_name.should.be.equal('Rue Jean Jaures')
+    invoice.emitter().zip_code.should.be.equal('75012')
+    invoice.emitter().city.should.be.equal('Paris')
+    invoice.emitter().country.should.be.equal('France')
+    invoice.emitter().phone.should.be.equal('01 00 00 00 00')
+    invoice.emitter().mail.should.be.equal('contact@dimsolution.com')
+    invoice.emitter().website.should.be.equal('www.dimsolution.com')
+    done()
+  })
 
+  it(`Object not auto-filled`, (done) => {
+    let invoice = generator.create()
+    invoice.emitter({
+      name: 'Dim Solution',
+      street_number: '15',
+      street_name: 'Rue Jean Jaures',
+      zip_code: '75012',
+      city: 'Paris',
+      country: 'France',
+      phone: '01 00 00 00 00',
+      mail: 'contact@dimsolution.com',
+      website: 'www.dimsolution.com'
+    })
+    invoice.recipient({
+      company_name: 'Receiver company',
+      first_name: 'Will',
+      last_name: 'Jameson',
+      street_number: '20',
+      street_name: 'Rue Victor Hugo',
+      zip_code: '77340',
+      city: 'Pontault-Combault',
+      country: 'France',
+      phone: '06 00 00 00 00',
+      mail: 'will.jameson@test.com'
+    })
+
+    invoice.recipient().company_name.should.be.equal('Receiver company')
+    invoice.recipient().first_name.should.be.equal('Will')
+    invoice.recipient().last_name.should.be.equal('Jameson')
+    invoice.recipient().street_number.should.be.equal('20')
+    invoice.recipient().street_name.should.be.equal('Rue Victor Hugo')
+    invoice.recipient().zip_code.should.be.equal('77340')
+    invoice.recipient().city.should.be.equal('Pontault-Combault')
+    invoice.recipient().country.should.be.equal('France')
+    invoice.recipient().phone.should.be.equal('06 00 00 00 00')
+    invoice.recipient().mail.should.be.equal('will.jameson@test.com')
+    invoice.emitter().name.should.be.equal(`Dim Solution`)
+    invoice.emitter().street_number.should.be.equal('15')
+    invoice.emitter().street_name.should.be.equal('Rue Jean Jaures')
+    invoice.emitter().zip_code.should.be.equal('75012')
+    invoice.emitter().city.should.be.equal('Paris')
+    invoice.emitter().country.should.be.equal('France')
+    invoice.emitter().phone.should.be.equal('01 00 00 00 00')
+    invoice.emitter().mail.should.be.equal('contact@dimsolution.com')
+    invoice.emitter().website.should.be.equal('www.dimsolution.com')
     done()
   })
 
