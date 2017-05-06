2dae6d4f118e31c4bf8f9bc897c2490338c89263
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-05-06 16:25:31 +0200

    feat(class/generator.js) generate html file

---
+++ src/class/generator.js
@@ -0,0 +1,105 @@
+import Recipient from './recipient'
+import Emitter from './emitter'
+import jade from 'jade'
+import path from 'path'
+import i18n from '../lib/i18n'
+import fs from 'fs'
+import moment from 'moment'
+
+export default class Generator {
+
+  constructor(config) {
+    this._emitter = (config.emitter) ? new Emitter(config.emitter) : new Emitter()
+    this._recipient = (config.recipient) ? new Recipient(config.recipient) : new Recipient()
+  }
+
+  recipient() {
+    return this._recipient
+  }
+
+  set recipient(value) {
+    this._recipient = value
+  }
+
+  emitter() {
+    return this._emitter
+  }
+
+  set emitter(value) {
+    this._emitter = value
+  }
+
+  get template() {
+    return this._template
+  }
+
+  set template(value) {
+    this._template = value
+  }
+
+  get lang() {
+    return (!this._lang) ? 'en' : this._lang
+  }
+
+  set lang(value) {
+    this._lang = value
+  }
+
+  get id() {
+    return this._id
+  }
+
+  set id(value) {
+    this._id = value
+  }
+
+  get reference() {
+    return this._reference
+  }
+
+  set reference(value) {
+    this._reference = value
+  }
+
+  _preCompileCommonTranslations() {
+    return {
+      table_information: i18n.__({phrase: 'table_information', locale: this.lang}),
+      table_description: i18n.__({phrase: 'table_description', locale: this.lang}),
+      table_tax: i18n.__({phrase: 'table_tax', locale: this.lang}),
+      table_quantity: i18n.__({phrase: 'table_quantity', locale: this.lang}),
+      table_price_without_taxes: i18n.__({phrase: 'table_price_without_taxes', locale: this.lang}),
+      table_price_without_taxes_unit: i18n.__({phrase: 'table_price_without_taxes_unit', locale: this.lang}),
+      table_note: i18n.__({phrase: 'table_note', locale: this.lang}),
+      table_total_without_taxes: i18n.__({phrase: 'table_total_without_taxes', locale: this.lang}),
+      table_total_taxes: i18n.__({phrase: 'table_total_taxes', locale: this.lang}),
+      table_total_with_taxes: i18n.__({phrase: 'table_total_with_taxes', locale: this.lang}),
+      footer: i18n.__({phrase: 'footer', locale: this.lang}),
+      moment: moment()
+    }
+  }
+
+  _compile(translations) {
+    let compiled = jade.compileFile(path.resolve('./static/order.jade'))
+    let html = compiled(Object.assign(this._preCompileCommonTranslations()))
+    fs.writeFile('test.html', html, translations) // Only for testing (during developments)
+    return html
+  }
+
+  getInvoice() {
+    return this._compile({
+      invoice_header_title: i18n.__({phrase: 'invoice_header_title', locale: this.lang}),
+      invoice_header_subject: i18n.__({phrase: 'invoice_header_subject', locale: this.lang}),
+      invoice_header_reference: i18n.__({phrase: 'invoice_header_reference', locale: this.lang}),
+      invoice_header_date: i18n.__({phrase: 'invoice_header_date', locale: this.lang})
+    })
+  }
+
+  getOrder() {
+    return this._compile({
+      order_header_title: i18n.__({phrase: 'order_header_title', locale: this.lang}),
+      order_header_subject: i18n.__({phrase: 'order_header_subject', locale: this.lang}),
+      order_header_reference: i18n.__({phrase: 'order_header_reference', locale: this.lang}),
+      order_header_date: i18n.__({phrase: 'order_header_date', locale: this.lang})
+    })
+  }
+}
