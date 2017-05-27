f5ac08509b179af6bc47a557e34051b4f007a641
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-05-27 15:27:06 +0200

    refact(class/common.js) methods moved to common class

--- src/class/common.js
+++ src/class/common.js
@@ -12,4 +12,28 @@ export default class Common {
     }
   }
 
+  /**
+   * @description Return number with padding
+   * @example if id = 10, return 0010
+   * @param num
+   * @param size
+   * @return {string}
+   * @private
+   */
+  pad(num, size) {
+    let output = num.toString()
+    while (output.length < size) output = `0${output}`
+    return output
+  }
+
+  /**
+   * @description Check if is a number
+   * @param n
+   * @returns {boolean}
+   * @private
+   */
+  isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+  }
+
 }
