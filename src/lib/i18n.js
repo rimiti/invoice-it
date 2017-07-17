ebf3dbd78762a0b92892ab361e83886b6a500986
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-07-17 16:30:12 +0200

    chore(lib/i18n.js) i18n is used like factory

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,11 +1,14 @@
-import i18n from 'i18n'
+import i18nFactory from 'i18n-factory'
+
+const i18n = i18nFactory.create()
 
 i18n.configure({
-    locales: ['en', 'fr'],
-    directory: `${__dirname}/../config/locales`,
-    defaultLocale: 'en',
-    logWarnFn: (message) => console.warn('warn', message),
-    logErrorFn: (message) => console.error('error', message)
+  locales: ['en', 'fr'],
+  directory: `${__dirname}/../config/locales`,
+  defaultLocale: 'en',
+  logWarnFn: (message) => console.warn('warn', message),
+  logErrorFn: (message) => console.error('error', message)
 })
+
 
 export default i18n
