67cfbeeffe1703d158540dd18e3e9ca032288113
Dimitri <contact@dimsolution.com>
2017-07-22 11:35:22 +0200

    feat(lib/generator.js) small changes

--- src/lib/generator.js
+++ src/lib/generator.js
@@ -2,25 +2,25 @@ import generator from '../class'
 
 generator.configure({
     "emitter": {
-      "name": `Your company`,
-      "street_number": `10`,
-      "street_name": `wall street`,
-      "zip_code": `77340`,
-      "city": `New York`,
-      "phone": `01 00 00 00 00`,
-      "mail": `contact@website.com`,
-      "website": `www.website.com`
+      "name": "Your company",
+      "street_number": "10",
+      "street_name": "wall street",
+      "zip_code": "77340",
+      "city": "New York",
+      "phone": "01 00 00 00 00",
+      "mail": "contact@website.com",
+      "website": "www.website.com"
     },
     "global": {
-      "logo": `http://placehold.it/230x70&text=logo`,
-      "order_reference_pattern": `$prefix{OR}$date{YYMM}$separator{-}$id{00000}`,
-      "invoice_reference_pattern": `$prefix{IN}$date{YYMM}$separator{-}$id{00000}`,
-      "order_template": `${__dirname}/../../static/order.pug`,
-      "order_note": ``,
-      "invoice_template": `${__dirname}/../../static/invoice.pug`,
-      "invoice_note": ``,
-      "date_format": `DD/MM/YYYY`,
-      "lang": `en`
+      "logo": "http://placehold.it/230x70&text=logo",
+      "order_reference_pattern": "$prefix{OR}$date{YYMM}$separator{-}$id{00000}",
+      "invoice_reference_pattern": "$prefix{IN}$date{YYMM}$separator{-}$id{00000}",
+      "order_template": __dirname + "/../../static/order.pug",
+      "order_note": "",
+      "invoice_template": __dirname + "/../../static/invoice.pug",
+      "invoice_note": "",
+      "date_format": "DD/MM/YYYY",
+      "lang": "en"
     }
   }
 )
